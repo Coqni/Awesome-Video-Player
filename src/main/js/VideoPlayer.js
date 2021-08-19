a2168adb620bf71c351eaef88928cabffe1378e0
maste150hhu <mastremolten2@gmail.com>
2021-08-19 14:26:27 +0200

    remove unused methods

--- src/main/js/VideoPlayer.js
+++ src/main/js/VideoPlayer.js
@@ -159,22 +159,3 @@ function get(videoPlayerId) {
         }
     }
 }
-/*
-function playVideo() {
-    preview = 1;
-    videoPlayer.play();
-}
-
-function pauseVideo() {
-    videoPlayer.pause();
-}
-
-function stopVideo() {
-    preview = 0;
-    videoPlayer.pause();
-    videoPlayer.currentTime = 1;
-}
-
-function changeVolume() {
-    videoPlayer.volume = document.getElementById("change-volume").value;
-}*/
